6b088b6f4b4be954f62c8a3a32d919b14da3985e
Claudio D. Morales <mclaudiod@gmail.com>
2023-12-07 19:09:24 -0300

    search arreglado y context agregado

--- TPintegrador/src/components/DrinksGrid.jsx
+++ TPintegrador/src/components/DrinksGrid.jsx
@@ -1,29 +1,31 @@
 import { DrinksCard } from "./DrinksCard";
 /* import drinks from "../data/tragos.json" */
-import "./DrinksGrid.css"
-import {get} from "../../utils/httpCliente";
-import {useState, useEffect} from "react";
+import "./DrinksGrid.css";
+import { get } from "../../utils/httpCliente";
+import { useState, useEffect } from "react";
 
+export const DrinksGrid = ({ searchResults }) => {
+  const [drinks, setDrinks] = useState([]);
 
-export const DrinksGrid = () => {
+  useEffect(() => {
+    if (searchResults.length > 0) {
+      setDrinks(searchResults);
+    } else {
+      get("/filter.php?c=Ordinary_Drink")
+        .then((data) => {
+          setDrinks(data.drinks);
+        })
+        .catch((error) => {
+          console.error("Error fetching drinks:", error);
+        });
+    }
+  }, [searchResults]);
 
-    const [drinks, setDrinks] = useState([])
-
-    
-    useEffect(() => {
-        get("/filter.php?c=Ordinary_Drink")
-          .then((data) => {
-            setDrinks(data.drinks);
-          })
-      }, []);
-    
-    return (
-
-        <ul className="drinksGrid">
-            
-            {drinks.map((drink) => (
-                <DrinksCard key={drink.idDrink} drink={drink}/>
-            ))}
-        </ul>
-    );
+  return (
+    <ul className="drinksGrid">
+      {drinks.map((drink) => (
+        <DrinksCard key={drink.idDrink} drink={drink} />
+      ))}
+    </ul>
+  );
 };
--- TPintegrador/src/components/NavBar.jsx
+++ TPintegrador/src/components/NavBar.jsx
@@ -1,49 +1,56 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import {Button, Container, Form, Nav, Navbar, NavDropdown} from 'react-bootstrap';
-import Brand from '../img/svg/logo-no-background.svg';
-import '../components/NavBar.css';
-
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Container,
+  Form,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+import Brand from "../img/svg/logo-no-background.svg";
+import "../components/NavBar.css";
+import SearchContext from "./SearchContext";
 
 function NavBar() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const { searchResults, setSearchResults } = useContext(SearchContext);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearch = async () => {
     try {
       const response = await fetch(
         `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
       );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
       const data = await response.json();
       setSearchResults(data.drinks || []);
-      setSearchTerm(''); // Clear the search bar after search
+      setSearchTerm(""); // Clear the search bar after search
     } catch (error) {
-      console.error('Error searching for cocktails:', error);
+      console.error("Error searching for cocktails:", error);
       setSearchResults([]);
     }
-  };
-  
-
-  const handleSearchResultClick = () => {
-    setSearchResults([]);
   };
 
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
-        <Container fluid>      
+        <Container fluid>
           <Link to="/" className="brand-logo">
             <img
-                src={Brand}
-                alt="Logo"
-                className="d-inline-block align-top logo-image"
-              />
+              src={Brand}
+              alt="Logo"
+              className="d-inline-block align-top logo-image"
+            />
           </Link>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
+              style={{ maxHeight: "100px" }}
               navbarScroll
             >
               <Nav.Link href="#action1">Home</Nav.Link>
@@ -77,35 +84,14 @@ function NavBar() {
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
-              <Button variant="outline-success" type="submit">Search</Button>
+              <Button variant="outline-success" type="submit">
+                Search
+              </Button>
             </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="search-results">
-        <ul className="drinksSearch">
-          {searchResults.map((drink) => (
-            <li key={drink.idDrink} className="drinksCard">
-              <Link
-                to={`/drink/${drink.idDrink}`}
-                className="drinksCardLink"
-                onClick={handleSearchResultClick}
-              >
-                <img
-                  src={drink.strDrinkThumb}
-                  alt={drink.strDrink}
-                  className="drinkImage"
-                />
-                <p>
-                  <span className="drinksName">{drink.strDrink}</span>
-                </p>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
     </div>
-    
   );
 }
 
