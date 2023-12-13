3b8551a5e54961d64a3fa9911ae20fdef15db58b
Rodritz <110502210+Rodritz@users.noreply.github.com>
2023-12-13 20:21:02 -0300

    Revert "arreglos css"
    This reverts commit fe7baf50e7888598fd91df24a1a861c9f7a3f36a.

--- TPintegrador/src/App.jsx
+++ TPintegrador/src/App.jsx
@@ -18,29 +18,27 @@ export const App = () => {
   const [searchResults, setSearchResults] = useState([]);
 
   return (
-    <div id="app" className="d-flex flex-column">
-      <BrowserRouter>
-        <SearchContext.Provider value={{ searchResults, setSearchResults }}>
-          <NavBar />
-          <Routes>          
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/home" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?i=Gin"/>} />
-            <Route path="/alcoholic" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?a=Alcoholic"/>} />
-            <Route path="/nonAlcoholic" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?a=Non_Alcoholic"/>} />  
-            <Route path="/ordinaryDrink" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?c=Ordinary_Drink"/>} /> 
-            <Route path="/cocktail" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?c=Cocktail"/>} /> 
-            <Route path="/cocktailGlass" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?g=Cocktail_glass"/>} /> 
-            <Route path="/champagneFlute" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?g=Champagne_flute"/>} /> 
-            <Route path="/drink/:idDrink" element={<DrinkDetails />} /> 
-            <Route path="/members" element={<Members/>} />
-            <Route path="/suscribe" element={<FormSubs/>} />          
-            <Route path="/create" element={<Create/>} />
-            <Route path="/edit/:id" element={<Edit/>} /> 
-            <Route path="/show" element= {<Show/>} />        
-          </Routes>
-          <Footer />
-        </SearchContext.Provider>
-      </BrowserRouter>
-  </div>
+    <BrowserRouter>
+      <SearchContext.Provider value={{ searchResults, setSearchResults }}>
+        <NavBar />
+        <Routes>          
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?i=Gin"/>} />
+          <Route path="/alcoholic" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?a=Alcoholic"/>} />
+          <Route path="/nonAlcoholic" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?a=Non_Alcoholic"/>} />  
+          <Route path="/ordinaryDrink" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?c=Ordinary_Drink"/>} /> 
+          <Route path="/cocktail" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?c=Cocktail"/>} /> 
+          <Route path="/cocktailGlass" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?g=Cocktail_glass"/>} /> 
+          <Route path="/champagneFlute" element={<DrinksGrid searchResults={searchResults} routePath="/filter.php?g=Champagne_flute"/>} /> 
+          <Route path="/drink/:idDrink" element={<DrinkDetails />} /> 
+          <Route path="/members" element={<Members/>} />
+          <Route path="/suscribe" element={<FormSubs/>} />          
+          <Route path="/create" element={<Create/>} />
+          <Route path="/edit/:id" element={<Edit/>} /> 
+          <Route path="/show" element= {<Show/>} />        
+        </Routes>
+        <Footer />
+      </SearchContext.Provider>
+    </BrowserRouter>
   );
 };
--- TPintegrador/src/pages/Members.jsx
+++ TPintegrador/src/pages/Members.jsx
@@ -1,27 +1,24 @@
-import "./Members.css";
+import "./Members.css"
 
 export const Members = () => {
-  const members = [
-    "Karina Soledad Meza - karinasolmeza83@gmail.com",
-    "Maria Sol Arroyo - mariasolarroyoc@gmail.com",
-    "Rodrigo Urquia - rodrigourq@gmail.com",
-    "Cristian Flores - dobalissimo@gmail.com",
-    "Maria Isabel Trettel - mariaisabeltrettel@gmail.com",
-    "Rosmery Torres - rosmery.torres10@gmail.com",
-    "Claudio Morales - mclaudiod@gmail.com",
-    "Enzo Valverde - enzovalverde@gmail.com",
-  ];
+    const members = ["Karina Soledad Meza - karinasolmeza83@gmail.com"
+    , "Maria Sol Arroyo - mariasolarroyoc@gmail.com"
+    , "Rodrigo Urquia - rodrigourq@gmail.com"
+    , "Cristian Flores - dobalissimo@gmail.com"
+    , "Maria Isabel Trettel - mariaisabeltrettel@gmail.com"
+    , "Rosmery Torres - rosmery.torres10@gmail.com"
+    , "Claudio Morales - mclaudiod@gmail.com"
+    , "Enzo Valverde - enzovalverde@gmail.com"]; 
 
   return (
-    <div className="members-container">
-      <div className="members">
-        <h1 className="titleMembers">Members G 9</h1>
-        <ul>
-          {members.map((member, index) => (
-            <li key={index}>{member}</li>
-          ))}
-        </ul>
-      </div>
+    <div className="members">
+        <h2>Members G 9</h2>
+      <ul>
+        {members.map((member, index) => (
+          <li key={index}>{member}</li>
+        ))}
+      </ul>
     </div>
-  );
-};
+    )
+}
+
