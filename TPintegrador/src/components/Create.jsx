2a319e55a0fffd5d77f8d0c9e0ac37aaf32cb77f
Rodritz <110502210+Rodritz@users.noreply.github.com>
2023-12-13 20:21:02 -0300

    Revert "arreglos css"
    This reverts commit fe7baf50e7888598fd91df24a1a861c9f7a3f36a.

--- TPintegrador/src/components/Create.jsx
+++ TPintegrador/src/components/Create.jsx
@@ -2,82 +2,64 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig/firebase.js";
-import "./Edit.css";
+import "./Edit.css"
 
 export const Create = () => {
-  const [nombre, setNombre] = useState("");
-  const [ingredientes, setIngredientes] = useState("");
-  const [instrucciones, setInstrucciones] = useState("");
-  const [categoria, setCategoria] = useState("");
 
-  const navigate = useNavigate();
+    const [nombre, setNombre] = useState("")
+    const [ingredientes, setIngredientes] = useState("")
+    const [instrucciones, setInstrucciones] = useState("")
+    const [categoria, setCategoria] = useState("")
 
-  const tragosCollection = collection(db, "tragos");
+    const navigate = useNavigate();
 
-  const createTrago = async (e) => {
-    e.preventDefault();
-    await addDoc(tragosCollection, {
-      nombre: nombre,
-      ingredientes: ingredientes,
-      instrucciones: instrucciones,
-      categoria: categoria,
-    });
-    navigate("/show");
-  };
+    const tragosCollection = collection(db, "tragos");
 
-  return (
-    <div className="edit_container">
-      <div className="row">
-        <div className="col">
-          <h1>CREAR TRAGO</h1>
-          <form onSubmit={createTrago}>
-            <div className="mb-3">
-              <label className="form-label">NOMBRE</label>
-              <input
-                className="form-control"
-                type="text"
-                onChange={(e) => setNombre(e.target.value)}
-              />
+    const createTrago = async (e) => {
+        e.preventDefault();
+        await addDoc(tragosCollection, {
+            nombre: nombre,
+            ingredientes: ingredientes,
+            instrucciones: instrucciones,
+            categoria: categoria,
+        });
+        navigate("/show");
+    };
+
+
+
+    return (
+        <div className="container edit_container">
+            <div className="row">
+                <div className="col">
+                    <h1>CREAR TRAGO</h1>
+                    <form onSubmit={createTrago}>
+                        <div className="mb-3">
+                            <label className="form-label" >NOMBRE</label>
+                            <input className="form-control" type="text" onChange={(e) => setNombre(e.target.value)} />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">INGREDIENTES</label>
+                            <input className="form-control" type="text" onChange={(e) => setIngredientes(e.target.value)} />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">INSTRUCCIONES</label>
+                            <input className="form-control" type="text" onChange={(e) => setInstrucciones(e.target.value)} />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">CATEGORIA</label>
+                            <input className="form-control" type="text" onChange={(e) => setCategoria(e.target.value)} />
+                        </div>
+
+                        <button type="submit" className="btn btn-secondary">ACEPTAR</button>
+                        <Link to="/show" className="btn btn-danger">CANCELAR</Link>
+                    </form>
+                </div>
             </div>
-
-            <div className="mb-3">
-              <label className="form-label">INGREDIENTES</label>
-              <input
-                className="form-control"
-                type="text"
-                onChange={(e) => setIngredientes(e.target.value)}
-              />
-            </div>
-
-            <div className="mb-3">
-              <label className="form-label">INSTRUCCIONES</label>
-              <input
-                className="form-control"
-                type="text"
-                onChange={(e) => setInstrucciones(e.target.value)}
-              />
-            </div>
-
-            <div className="mb-3">
-              <label className="form-label">CATEGORIA</label>
-              <input
-                className="form-control"
-                type="text"
-                onChange={(e) => setCategoria(e.target.value)}
-              />
-            </div>
-
-            <div className="btn-edit-create">
-              <button type="submit" className="btn btn-secondary">
-                ACEPTAR
-              </button>
-              <Link to="/show" className="btn btn-danger">
-                CANCELAR
-              </Link>
-            </div>
-          </form>
         </div>
-      </div>
-    </div>
-  );
+
+    )
 };
--- TPintegrador/src/pages/LandingPage.jsx
+++ TPintegrador/src/pages/LandingPage.jsx
@@ -2,20 +2,21 @@
 import SearchContext from "../components/SearchContext"; */
 import "./LandingPage.css";
 
-export const LandingPage = () => {
-  /* const { searchResults } = useContext(SearchContext); */
 
+
+export const LandingPage = () => {
+  /* const { searchResults } = useContext(SearchContext); */ 
+
+  
   return (
-    <div className="contain">
-      <div className="containerNeon">
-        <h1 className="neon">•. Welcome to .•</h1>
-        <p className="neon2">Cocktails!</p>
-        <p className="neon3">
-          <span className="flicker1">★</span>
-          <span className="flicker2">★</span>
-          <span className="flicker3">★</span>
-        </p>
-      </div>
+    <div className="containerNeon">
+      <h1 className="neon">•. Welcome to .•</h1>
+      <p className="neon2">Cocktails!</p>
+      <p className="neon3">
+        <span className="flicker1">★</span>
+        <span className="flicker2">★</span>
+        <span className="flicker3">★</span>
+      </p>
     </div>
-  );
-};
+  )
+}
