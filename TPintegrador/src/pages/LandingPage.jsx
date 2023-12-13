fe7baf50e7888598fd91df24a1a861c9f7a3f36a
Rosmery Torres <rosmery.torres10@gmail.com>
2023-12-13 01:21:08 -0300

    arreglos css

--- TPintegrador/src/pages/LandingPage.jsx
+++ TPintegrador/src/pages/LandingPage.jsx
@@ -2,21 +2,20 @@
 import SearchContext from "../components/SearchContext"; */
 import "./LandingPage.css";
 
-
-
 export const LandingPage = () => {
-  /* const { searchResults } = useContext(SearchContext); */ 
+  /* const { searchResults } = useContext(SearchContext); */
 
-  
   return (
-    <div className="containerNeon">
-      <h1 className="neon">•. Welcome to .•</h1>
-      <p className="neon2">Cocktails!</p>
-      <p className="neon3">
-        <span className="flicker1">★</span>
-        <span className="flicker2">★</span>
-        <span className="flicker3">★</span>
-      </p>
+    <div className="contain">
+      <div className="containerNeon">
+        <h1 className="neon">•. Welcome to .•</h1>
+        <p className="neon2">Cocktails!</p>
+        <p className="neon3">
+          <span className="flicker1">★</span>
+          <span className="flicker2">★</span>
+          <span className="flicker3">★</span>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
